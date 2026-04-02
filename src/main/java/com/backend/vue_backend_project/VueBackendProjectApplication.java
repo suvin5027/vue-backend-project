afe84d941ec38509d5b26cc207402d7996fda16f
suvin5027 <suvin5027@gmail.com>
2026-04-02 14:14:04 +0900

    first commit

---
+++ src/main/java/com/backend/vue_backend_project/VueBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vue_backend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/vue_backend_project/VueBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vue_backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
