01e67b57aa217e8afe03029e8ef0161d1e809ff5
suvin5027 <suvin5027@gmail.com>
2026-04-03 10:16:04 +0900

    주석 수정

--- src/main/java/com/backend/vue_backend_project/controller/TempController.java
+++ src/main/java/com/backend/vue_backend_project/controller/TempController.java
@@ -14,11 +14,11 @@ import com.backend.vue_backend_project.service.TempService;
 
 @RestController // 1. 데이터를 보내주기 위해 추가!
 @RequestMapping("/api") // 2. API 주소 설정 (통행증!)
-@CrossOrigin(origins = "http://localhost:5173") // 3. Vue 접속 허용 (통행증!)
+@CrossOrigin(origins = "http://localhost:5173") // 3. Vue 접속 허용
 public class TempController {
 
 	@Autowired
-	private TempService tempService; // 4. 서비스 연결 (통행증!)
+	private TempService tempService; // 4. 서비스 연결
 
 	@GetMapping("/test")
 	public List<TempDTO> getTempList() {
--- src/main/java/com/backend/vue_backend_project/repository/TempRepository.java
+++ src/main/java/com/backend/vue_backend_project/repository/TempRepository.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.backend.vue_backend_project.entity.TempEntity;
 
 @Repository
-// TempRepository는 TempEntity를 관리하는 JPA 리포지토리 인터페이스입니다.
+// TempRepository는 TempEntity를 관리하는 JPA 리포지토리 인터페이스
 public interface TempRepository extends JpaRepository<TempEntity, Long> {
-	// JpaRepository는 기본적인 CRUD 메서드를 제공하므로, 별도의 메서드를 추가하지 않아도 됩니다.
-	//
+	// JpaRepository는 기본적인 CRUD 메서드를 제공하므로, 별도의 메서드를 추가하지 않아도 됨.
 }
